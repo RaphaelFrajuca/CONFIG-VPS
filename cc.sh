cc2b006019978dea7b9e749f80a71061ad597e94
Raphael Frajuca <olocogameplays552@gmail.com>
2016-11-06 01:37:35 -0200

    Mudado Diretorios para /etc/ccgen/

--- cc.sh
+++ cc.sh
@@ -8,8 +8,8 @@
 # Esse Script Utiliza Outro Script Feito por MIM Usando o baluhn para calcular o digito verificador.
 
 bin=$(echo $1 | awk -F "x" '{print $1}')
-echo $bin > bin.beta
-seq 1 $2 > limite.beta
+echo $bin > /etc/ccgen/bin.beta
+seq 1 $2 > /etc/ccgen/limite.beta
 while read limite
 do
 while read inn
@@ -19,5 +19,5 @@ numerogen=$2
 seq=$(seq 1 $numerogen)
 limite2=$(seq 1 $limite)
 echo $inn$cc$(python /etc/ccgen/gen.py $cc) $((($RANDOM % 10) + 3))'/'20$(shuf -i16-22 -n1) $(($RANDOM % 10))$(($RANDOM % 10))$(($RANDOM % 10)) | awk '{print }'
-done < bin.beta
-done < limite.beta
+done < /etc/ccgen/bin.beta
+done < /etc/ccgen/limite.beta
